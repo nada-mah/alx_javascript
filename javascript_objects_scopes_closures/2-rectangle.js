59b1c3c0c64e6ed9c91563f1e1c60845a7f696f8
nada-mah <cutenada.lara4@gmail.com>
2024-01-18 18:59:31 +0200

    new file:  /2-rectangle.js

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+class Rectangle  {
+    constructor(w,h) 
+    {
+        if(h<=0 || w <=0 || w === undefined || h === undefined)
+        {
+            return
+        }
+        this.width = w
+        this.height = h
+    }
+  }
+
+
+
+module.exports = Rectangle;
