484535b68ee3b1669b8b40cf9a41f79f712ec664
nada-mah <cutenada.lara4@gmail.com>
2024-01-18 19:35:41 +0200

    new file:   /6-square.js

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Rectangle = require("./4-rectangle")
+
+class Square extends Rectangle
+{
+    constructor(size)
+    {
+        super(size,size)
+    }
+    charPrint(c)
+    {
+        if(c == undefined)
+        {
+            this.print()
+        }
+        else
+        {
+            for(let i=0; i<this.height; i++){
+                for(let i=0; i<this.width; i++){
+                    process.stdout.write(c)
+                }
+                console.log()
+            }
+        }
+    }
+}
+
+module.exports = Square
