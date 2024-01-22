90da2e8a3cc3b5bac92f7c875cee6c0e90dc01fb
nada-mah <cutenada.lara4@gmail.com>
2024-01-22 16:07:14 +0200

    task3

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,13 +4,14 @@ const fs = require('fs')
 
 let URL = process.argv[2]
 let bodydestination = process.argv[3]
-
+data = ''
 
 req.get(URL, {encoding : 'utf-8'})
     .on('data', data => {
         fs.writeFile(bodydestination, data, (err) => {
-
-            // if (err) throw err;
+            if (err) throw err;
         })
     })
              
+    
+    
