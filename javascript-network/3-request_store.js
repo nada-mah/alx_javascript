6a8e7597fe5c029abf9d9ab62f96fa70033b448f
nada-mah <cutenada.lara4@gmail.com>
2024-01-22 16:03:33 +0200

    no err msg

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -9,9 +9,8 @@ let bodydestination = process.argv[3]
 req.get(URL, {encoding : 'utf-8'})
     .on('data', data => {
         fs.writeFile(bodydestination, data, (err) => {
- 
-            // In case of a error throw err.
-            if (err) throw err;
+
+            // if (err) throw err;
         })
     })
              
