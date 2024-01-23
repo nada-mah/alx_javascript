ad69f9846b1455969bf931207f28485df6221bfd
nada-mah <cutenada.lara4@gmail.com>
2024-01-23 19:53:28 +0200

    tasl4

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -8,28 +8,18 @@ options = {
     'url' : URL,
 
 }
-let tasksCompleted = {
-    '1': 0,
-    '2': 0,
-    '3': 0,
-    '4': 0,
-    '5': 0,
-    '6': 0,
-    '7': 0,
-    '8': 0,
-    '9': 0,
-    '10': 0
-
-}
+let tasksCompleted = {}
 
 request(options, (err, res)=>{
     if(err) console.log(err)
     record = JSON.parse(res.body)
     for(let i = 0; i<record.length;i++){
         if(record[i]['completed'] ){
-            if(record[i]['userId'] in tasksCompleted){
-                tasksCompleted[record[i]['userId']]+=1
-            }
+                if(record[i]['userId'] in tasksCompleted){
+                    tasksCompleted[record[i]['userId']]+=1
+                }else{
+                    tasksCompleted[record[i]['userId']]=1
+                }
         }
     }
     console.log(tasksCompleted)
